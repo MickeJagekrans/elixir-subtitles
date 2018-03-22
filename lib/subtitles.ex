8479746047312ff17bfd01cd66b113ecc3260d38
Mikael Jagekrans <mikael.jagekrans@magine.com>
2018-03-22 14:34:46 +0100

    code formatting

--- lib/subtitles.ex
+++ lib/subtitles.ex
@@ -1,4 +1,6 @@
 defmodule Subtitles do
+  @srt_cue_matcher ~r/\n?1\n(\d{2}:){2}\d{2},\d{3} --> (\d{2}:){2}\d{2},\d{3}(\n.+)+\n\n/
+
   def get_format(subtitle) do
     subtitle = subtitle |> normalize_line_endings()
 
@@ -10,8 +12,5 @@ defmodule Subtitles do
   defp normalize_line_endings(subtitle), do: Regex.replace(~r/\r\n?/, subtitle, "\n")
 
   defp is_vtt?(subtitle), do: subtitle |> String.starts_with?("WEBVTT")
-  defp is_srt?(subtitle) do
-    matcher = ~r/\n?1\n(\d{2}:){2}\d{2},\d{3} --> (\d{2}:){2}\d{2},\d{3}(\n.+)+\n\n/
-    Regex.match?(matcher, subtitle)
-  end
+  defp is_srt?(subtitle), do: @srt_cue_matcher |> Regex.match?(subtitle)
 end
