7df05904f1269182dcba84068ea21f02f4f5cc1f
Mikael Jagekrans <mikael.jagekrans@magine.com>
2018-03-26 15:34:46 +0200

    added vtt parser and tests

--- lib/parsers/vtt_parser.ex
+++ lib/parsers/vtt_parser.ex
@@ -1,5 +1,38 @@
 defmodule Subtitles.VttParser do
+  @time_pattern ~r/\d{2}:\d{2}:\d{2}\.\d{3}/
+  @unsupported_blocks ["WEBVTT", "NOTE", "REGION", "STYLE"]
+
   def parse(subtitle) do
     subtitle
+    |> String.split("\n\n", trim: true)
+    |> filter_unsupported_blocks()
+    |> build_cues()
+  end
+
+  defp filter_unsupported_blocks(blocks) do
+    blocks |> Enum.reject(&String.starts_with?(&1, @unsupported_blocks))
+  end
+
+  defp build_cues(blocks) do
+    blocks |> Enum.map(fn block ->
+      block
+      |> String.split("\n")
+      |> remove_cue_headers()
+      |> make_cue()
+    end)
+  end
+
+  defp remove_cue_headers(cue = [first|rest]) do
+    @time_pattern |> Regex.match?(first) && cue || rest
+  end
+
+  defp make_cue([timings|parts]) do
+    [[from], [to]|_] = @time_pattern |> Regex.scan(timings)
+
+    Subtitle.new(from, to, parts |> remove_timestamps())
+  end
+
+  defp remove_timestamps(parts) do
+    parts |> Enum.map(&Regex.replace(~r/<\d{2}:\d{2}:\d{2}.\d{3}>/, &1, ""))
   end
 end
--- spec/parsers/vtt_parser_spec.exs
+++ spec/parsers/vtt_parser_spec.exs
@@ -1,3 +1,46 @@
 defmodule Subtitles.VttParserSpec do
   use ESpec
+
+  describe "#parse" do
+    subject do: sub() |> Subtitles.VttParser.parse()
+
+    describe "valid sub" do
+      let :sub, do: File.read!("spec/fixtures/test.vtt")
+      let :expected, do: [
+        %Subtitle{
+          from: ~T[00:00:00.000],
+          to: ~T[00:00:10.000],
+          parts: [
+            %SubtitlePart{text_data: "Example entry 1: Hello <b>world</b>."}
+          ]
+        },
+        %Subtitle{
+          from: ~T[00:00:25.000],
+          to: ~T[00:00:35.000],
+          parts: [
+            %SubtitlePart{text_data: "Example entry 2: Another entry."},
+            %SubtitlePart{text_data: "This one has multiple lines."}
+          ]
+        },
+        %Subtitle{
+          from: ~T[00:01:03.000],
+          to: ~T[00:01:06.500],
+          parts: [
+            %SubtitlePart{text_data: "Example entry 3: That stuff to the right of the timestamps are cue settings."}
+          ]
+        },
+        %Subtitle{
+          from: ~T[00:03:10.000],
+          to: ~T[00:03:20.000],
+          parts: [
+            %SubtitlePart{text_data: "Example entry 4: Entries can even include timestamps."},
+            %SubtitlePart{text_data: "For example:This becomes visible five seconds"},
+            %SubtitlePart{text_data: "after the first part."}
+          ]
+        }
+      ]
+
+      it do: should eq expected()
+    end
+  end
 end
