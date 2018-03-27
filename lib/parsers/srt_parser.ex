39908345df69b3a96a9c0da25024f33ead49dd79
Mikael Jagekrans <mikael.jagekrans@magine.com>
2018-03-27 17:26:42 +0200

    fixed function name mismatch

--- lib/parsers/srt_parser.ex
+++ lib/parsers/srt_parser.ex
@@ -1,9 +1,9 @@
 defmodule Subtitles.SrtParser do
   def parse(subtitle) do
-    subtitle |> String.split("\n\n", trim: true) |> Enum.map(&format_cue/1)
+    subtitle |> String.split("\n\n", trim: true) |> Enum.map(&parse_cue/1)
   end
 
-  defp parse_cue(cue), do: cue |> String.split("\n") |> make_cue()
+  defp parse_cue(cue), do: cue |> String.split("\n") |> make_subtitle()
 
   defp make_subtitle([_, timing|parts]) do
     [from, to] = timing |> String.split(" --> ")
