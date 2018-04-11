defa753ba70b4ec40e166bacee591b49614deece
Mikael Jagekrans <mikael.jagekrans@magine.com>
2018-04-11 15:38:08 +0200

    added required data to mix.exs

--- mix.exs
+++ mix.exs
@@ -6,7 +6,10 @@ defmodule Subtitles.Mixfile do
       app: :subtitles,
       version: "0.1.0",
       elixir: "~> 1.5",
+      build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
+      description: description(),
+      package: package(),
       preferred_cli_env: [espec: :test],
       deps: deps()
     ]
@@ -18,9 +21,24 @@ defmodule Subtitles.Mixfile do
     ]
   end
 
+  defp description do
+    """
+    Library for manipulating and converting subtitles
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Mikael Jagekrans"],
+      links: %{"GitHub" => "https://github.com/MickeJagekrans/elixir-subtitles"},
+      licenses: ["MIT"]
+    ]
+  end
+
   defp deps do
     [
-      {:espec, "~> 1.5.0", only: :test}
+      {:espec, "~> 1.5.0", only: :test},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 end
